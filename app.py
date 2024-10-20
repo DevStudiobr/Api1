51a8dace8839d2845117e9a563099948632e1bd5
radent <118765402+DevStudiobr@users.noreply.github.com>
2024-10-20 14:44:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify(message="Bem-vindo(a) à API hospedada no GitHub!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
